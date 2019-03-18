bb770ea776b4e0edbf3646cc53e0b63c0dd3772a
Ziziou91 <joshgray91@gmail.com>
2019-03-18 21:57:06 +0000

    getAttendees returns object of all EC attendees

---
+++ .ignore/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  eventbrite: 'YINZY7CT7OM6PKWDBD4T',
+  hubspot: '805bf4e4-41a0-4f27-bb41-f4f0eb85fd33',
+  ycbm: {
+    username: 'hello@northcoders.com',
+    password: 'Tw1nkleT0e5oy'
+  }
+};
---
+++ index.js
@@ -0,0 +1,82 @@
+const request = require('request-promise');
+const {
+  getYCBMBookings,
+  getIntervieweesEmails
+} = require('./lib/youCanBookMe');
+const { getInterviewees } = require('./lib/hubspotData');
+const KEYS = require('./.ignore/keys');
+
+const getCodeWarsScore = async codewarsURL => {
+  let username = codewarsURL.split('/');
+  username = username[username.length - 1];
+  const codewarsInfo = await request(
+    `https://www.codewars.com/api/v1/users/${username}`,
+    { json: true },
+    (err, res, body) => {
+      return body;
+    }
+  );
+  return codewarsInfo.honor;
+};
+
+const getInterviewObject = async (intervieweesArr, liveBookings) => {
+  const codewarsScore = await intervieweesArr.map(async interviewee => {
+    const interview = liveBookings.find(booking =>
+      booking.title.includes(interviewee.properties.email.value)
+    );
+    const campus = interviewee.properties.interview_booking_link.value.includes(
+      'leeds'
+    )
+      ? 'Leeds'
+      : 'Manchester';
+    let interviewObj = {
+      name: `${interviewee.properties.firstname.value} ${
+        interviewee.properties.lastname.value
+      }`,
+      interview: interview.startsAt,
+      campus: campus,
+      'codewars score': 'No CodeWars info :('
+    };
+    if (interviewee.properties.codewars_username) {
+      let username = interviewee.properties.codewars_username.value.split('/');
+      username = username[username.length - 1];
+      const score = await request(
+        `https://www.codewars.com/api/v1/users/${username}`,
+        { json: true },
+        (err, res, body) => {
+          return body;
+        }
+      );
+      interviewObj['codewars score'] = score.honor;
+      interviewObj['codewars profile'] =
+        interviewee.properties.codewars_username.value;
+    }
+
+    return interviewObj;
+  });
+  return Promise.all(codewarsScore).then(value => value);
+};
+
+const getAttendees = async auth => {
+  const liveBookings = await getYCBMBookings(auth.ycbm);
+  const emails = getIntervieweesEmails(liveBookings);
+  const interviewees = await getInterviewees(emails, auth.hubspot);
+  const intervieweesArr = Object.values(interviewees);
+  const codewarsScore = await getInterviewObject(intervieweesArr, liveBookings);
+  console.log(codewarsScore);
+};
+
+getAttendees(KEYS);
+//getCodeWarsScore('fatoumi');
+
+// const interview = liveBookings.find(booking =>
+//     booking.title.includes(interviewee.properties.email.value)
+//   );
+// const interviewObj = {
+//   name: `${interviewee.properties.firstname} ${
+//     interviewee.properties.lastname
+//   }`,
+//   time: interview.startsAt,
+//   'codewars username': '',
+//   'codewars score': ''
+// };
