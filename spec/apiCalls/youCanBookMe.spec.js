d662f3ce68faefcf20a62136da168411bdd229fe
Ziziou91 <joshgray91@gmail.com>
2019-03-30 17:25:19 +0000

    implement testing for youCanBookMe spec

--- spec/apiCalls/youCanBookMe.spec.js
+++ spec/apiCalls/youCanBookMe.spec.js
@@ -1,16 +1,61 @@
 const { expect } = require('chai');
 const KEYS = require('../../.ignore/keys');
-const { getAllBookings } = require('../../lib/apiCalls/youCanBookMe');
+const {
+  getAllBookings,
+  filterPastInterviews
+} = require('../../lib/apiCalls/youCanBookMe');
 const { calendarIDs, invalidCalendarIDs } = require('../mocks/mocks');
+const {
+  bookings,
+  pastBookings,
+  futureBookings
+} = require('../mocks/youCanBookMe');
 
 describe('getAllBookings', function() {
-  this.timeout(6000);
+  this.timeout(8000);
   it('should return an array of YCBM bookings', async () => {
     const result = await getAllBookings(calendarIDs, KEYS.ycbm);
     expect(result).to.be.an('array');
+    expect(result).to.be.an('array');
+  });
+  it('returns an empty array when passed an empty array of bookings', async () => {
+    const result = await getAllBookings([], KEYS.ycbm);
+    expect(result).to.eql([]);
   });
   it('should handle invalid calendarIDs', async () => {
     const error = await getAllBookings(invalidCalendarIDs, KEYS.ycbm);
     expect(error.statusCode).to.equal(401);
+  });
+});
+
+describe.only('filterPastInterviews', function() {
+  it('should return an empty array when bookings an empty array', () => {
+    const result = filterPastInterviews([]);
+    expect(result).to.be.an('array');
+    expect(result).to.eql([]);
+  });
+  it('should return an empty array when all bookings are in the past', () => {
+    const result = filterPastInterviews(pastBookings);
+    expect(result).to.be.an('array');
+    expect(result).to.eql([]);
+  });
+  it('should return an array of the same length when all bookings in the future', () => {
+    const result = filterPastInterviews(futureBookings);
+    expect(result).to.be.an('array');
+    expect(result.length).to.eql(futureBookings.length);
+  });
+  it('should return an array of the same length when all bookings in the future', () => {
+    const result = filterPastInterviews(futureBookings);
+    expect(result).to.be.an('array');
+    expect(result.length).to.eql(futureBookings.length);
+  });
+  it('should return only the future bookings when passed a mixed array', () => {
+    const result = filterPastInterviews(bookings);
+    const title1 = 'Booked: John Doe jdoe@gmail.com';
+    const title2 = 'Booked: John Smith smithjr@gmail.com';
+    expect(result).to.be.an('array');
+    expect(result.length).to.eql(2);
+    expect(result[0].title).to.equal(title1);
+    expect(result[1].title).to.equal(title2);
   });
 });
---
+++ spec/mocks/youCanBookMe.js
@@ -0,0 +1,122 @@
+const bookings = [
+  {
+    id: '14a9ec6f-aba2-4a6a-9a7e-7e052f0e0b03',
+    title: 'Booked: John Doe jdoe@gmail.com',
+    accountId: '83febcfd-f898-401a-bf0f-2dae1d7990e5',
+    profileId: '9824f309-1f6a-40e2-8843-51a8cdc88632',
+    createdAt: '2019-03-26T09:27:11Z',
+    startsAt: '2050-03-29T10:00:00',
+    endsAt: '2050-03-29T11:30:00',
+    tentative: 'NOT_USED',
+    timeZone: 'Europe/London',
+    cancelled: false
+  },
+  {
+    id: '673516f9-2d2e-4480-b511-4a620efe4902',
+    title: 'Booked: Jane Doe Janedoe@gmail.com',
+    accountId: '83febcfd-f898-401a-bf0f-2dae1d7990e5',
+    profileId: '9824f309-1f6a-40e2-8843-51a8cdc88632',
+    createdAt: '2019-03-11T19:46:15Z',
+    startsAt: '2019-03-29T14:00:00',
+    endsAt: '2019-03-29T15:30:00',
+    tentative: 'NOT_USED',
+    timeZone: 'Europe/London',
+    cancelled: false
+  },
+  {
+    id: 'd71348ee-c5ae-4715-9e96-a6a962491af4',
+    title: 'Booked: John Smith smithjr@gmail.com',
+    accountId: '83febcfd-f898-401a-bf0f-2dae1d7990e5',
+    profileId: '9824f309-1f6a-40e2-8843-51a8cdc88632',
+    createdAt: '2019-03-14T18:13:03Z',
+    startsAt: '2050-04-03T12:00:00',
+    endsAt: '2050-04-03T13:30:00',
+    tentative: 'NOT_USED',
+    timeZone: 'Europe/London',
+    cancelled: false
+  }
+];
+
+const pastBookings = [
+  {
+    id: '14a9ec6f-aba2-4a6a-9a7e-7e052f0e0b03',
+    title: 'Booked: John Doe jdoe@gmail.com',
+    accountId: '83febcfd-f898-401a-bf0f-2dae1d7990e5',
+    profileId: '9824f309-1f6a-40e2-8843-51a8cdc88632',
+    createdAt: '2019-03-26T09:27:11Z',
+    startsAt: '2018-03-29T10:00:00',
+    endsAt: '2018-03-29T11:30:00',
+    tentative: 'NOT_USED',
+    timeZone: 'Europe/London',
+    cancelled: false
+  },
+  {
+    id: '673516f9-2d2e-4480-b511-4a620efe4902',
+    title: 'Booked: Jane Doe Janedoe@gmail.com',
+    accountId: '83febcfd-f898-401a-bf0f-2dae1d7990e5',
+    profileId: '9824f309-1f6a-40e2-8843-51a8cdc88632',
+    createdAt: '2019-03-11T19:46:15Z',
+    startsAt: '2018-03-29T14:00:00',
+    endsAt: '2018-03-29T15:30:00',
+    tentative: 'NOT_USED',
+    timeZone: 'Europe/London',
+    cancelled: false
+  },
+  {
+    id: 'd71348ee-c5ae-4715-9e96-a6a962491af4',
+    title: 'Booked: John Smith smithjr@gmail.com',
+    accountId: '83febcfd-f898-401a-bf0f-2dae1d7990e5',
+    profileId: '9824f309-1f6a-40e2-8843-51a8cdc88632',
+    createdAt: '2019-03-14T18:13:03Z',
+    startsAt: '2018-04-03T12:00:00',
+    endsAt: '2018-04-03T13:30:00',
+    tentative: 'NOT_USED',
+    timeZone: 'Europe/London',
+    cancelled: false
+  }
+];
+
+const futureBookings = [
+  {
+    id: '14a9ec6f-aba2-4a6a-9a7e-7e052f0e0b03',
+    title: 'Booked: John Doe jdoe@gmail.com',
+    accountId: '83febcfd-f898-401a-bf0f-2dae1d7990e5',
+    profileId: '9824f309-1f6a-40e2-8843-51a8cdc88632',
+    createdAt: '2019-03-26T09:27:11Z',
+    startsAt: '2050-03-29T10:00:00',
+    endsAt: '2050-03-29T11:30:00',
+    tentative: 'NOT_USED',
+    timeZone: 'Europe/London',
+    cancelled: false
+  },
+  {
+    id: '673516f9-2d2e-4480-b511-4a620efe4902',
+    title: 'Booked: Jane Doe Janedoe@gmail.com',
+    accountId: '83febcfd-f898-401a-bf0f-2dae1d7990e5',
+    profileId: '9824f309-1f6a-40e2-8843-51a8cdc88632',
+    createdAt: '2019-03-11T19:46:15Z',
+    startsAt: '2050-03-29T14:00:00',
+    endsAt: '2050-03-29T15:30:00',
+    tentative: 'NOT_USED',
+    timeZone: 'Europe/London',
+    cancelled: false
+  },
+  {
+    id: 'd71348ee-c5ae-4715-9e96-a6a962491af4',
+    title: 'Booked: John Smith smithjr@gmail.com',
+    accountId: '83febcfd-f898-401a-bf0f-2dae1d7990e5',
+    profileId: '9824f309-1f6a-40e2-8843-51a8cdc88632',
+    createdAt: '2019-03-14T18:13:03Z',
+    startsAt: '2050-04-03T12:00:00',
+    endsAt: '2050-04-03T13:30:00',
+    tentative: 'NOT_USED',
+    timeZone: 'Europe/London',
+    cancelled: false
+  }
+];
+
+module.exports = {
+  pastBookings,
+  futureBookings,
+  bookings
+};
