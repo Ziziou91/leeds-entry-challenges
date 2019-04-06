648e019a6f495f2176873b3da3e23911ca544d71
Ziziou91 <joshgray91@gmail.com>
2019-04-06 22:38:28 +0100

    create testing arrays with appropiate dates

--- spec/logic/formatInterviews.spec.js
+++ spec/logic/formatInterviews.spec.js
@@ -1,13 +1,32 @@
 const { expect } = require('chai');
+const moment = require('moment');
 const {
   getFutureDate,
   filterGivenWeeksIVs
 } = require('../../lib/logic/formatInterviews');
 const { codewarsArray } = require('../mocks/formatInterviews');
 
+const createInterviewTimes = (codewarsArray, daysToAdd) => {
+  return codewarsArray.map(appointment => {
+    appointment.interview = new Date(
+      moment()
+        .add(daysToAdd, 'days')
+        .format()
+    );
+    return appointment;
+  });
+};
+
+const thisWeeksInterviews = createInterviewTimes(codewarsArray, 0);
+const nextWeeksInterviews = createInterviewTimes(codewarsArray, 7);
+
 describe.only('filterGivenWeeksIVs', function() {
   it('returns the correct day', () => {
-    const date = filterGivenWeeksIVs('next-week', codewarsArray);
-    //console.log(date);
+    const [startDate, endDate, weeksIVs] = filterGivenWeeksIVs(
+      'next-week',
+      codewarsArray
+    );
+    //console.log(thisWeeksInterviews);
+    console.log(nextWeeksInterviews);
   });
 });
