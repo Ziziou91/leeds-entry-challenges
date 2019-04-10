6739c1499aca63fc884f26b22e9795e6e8d8f8f3
Ziziou91 <joshgray91@gmail.com>
2019-04-10 11:50:09 +0100

    implement testing for invalid codewars URL

--- spec/apiCalls/codewars.spec.js
+++ spec/apiCalls/codewars.spec.js
@@ -1,10 +1,29 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const expect = chai.expect;
+chai.use(chaiAsPromised);
 const { getCodeWarsScore } = require('../../lib/apiCalls/codewars');
-const { properties, interviewObj } = require('../mocks/codewars');
+const {
+  hubspotContact,
+  invalidCWHubspotContact,
+  interviewObj
+} = require('../mocks/codewars');
 
 describe.only('getCodeWarsScore', function() {
-  it('returns an interview object', async () => {
-    const interviewees = await getCodeWarsScore(properties, interviewObj);
-    expect(interviewees).to.be.an('object');
+  this.timeout(10000);
+  it('returns an interview object when given valid arguments', async () => {
+    const interviewee = await getCodeWarsScore(hubspotContact, interviewObj);
+    expect(interviewee).to.be.an('object');
+    expect(interviewee['codewars score']).to.be.a('number');
+    expect('interview' in interviewee).to.equal(true);
+  });
+  it('handles inputs with an invalid codewars URL', async () => {
+    await expect(
+      getCodeWarsScore(invalidCWHubspotContact, interviewObj)
+    ).to.be.rejectedWith(
+      `StatusCodeError: 404 - {"success":false,"reason":"not found"}\n${
+        invalidCWHubspotContact.codewars_username.value
+      } is not a valid CodeWars URL`
+    );
   });
 });
--- spec/mocks/codewars.js
+++ spec/mocks/codewars.js
@@ -1,4 +1,4 @@
-const properties = {
+const hubspotContact = {
   num_unique_conversion_events: { value: '1' },
   hs_email_optout_4467451: { value: '' },
   createdate: { value: '1553619891962' },
@@ -147,6 +147,155 @@ const properties = {
   lifecyclestage: { value: 'opportunity' }
 };
 
+const invalidCWHubspotContact = {
+  num_unique_conversion_events: { value: '1' },
+  hs_email_optout_4467451: { value: '' },
+  createdate: { value: '1553619891962' },
+  hs_analytics_first_referrer: { value: 'https://www.google.com/' },
+  hs_calculated_phone_number_country_code: { value: 'GB' },
+  hs_email_last_click_date: { value: '1554747756621' },
+  recent_conversion_event_name: {
+    value:
+      'Apply | Northcoders Coding Bootcamps, Leeds and Manchester: Application Form Official - November 2018'
+  },
+  hubspot_owner_id: { value: '30247343' },
+  hs_email_optout_4113126: { value: '' },
+  num_associated_deals: { value: '1' },
+  marketing_lifecycle_stage: { value: 'B2C Application' },
+  hs_analytics_num_page_views: { value: '18' },
+  hs_email_optout_4468136: { value: '' },
+  hs_sales_email_last_opened: { value: '1554491042559' },
+  twilio_book_your_interview_trigger: { value: 'true' },
+  recent_deal_close_date: { value: '' },
+  hs_email_optout_4467449: { value: '' },
+  lastname: { value: 'Gray' },
+  phone: { value: '' },
+  hs_predictivecontactscore_v2: { value: '14.74' },
+  stay_up_to_date: { value: 'false' },
+  currentlyinworkflow: { value: 'true' },
+  hs_analytics_num_event_completions: { value: '0' },
+  hs_lifecyclestage_lead_date: { value: '1553619892303' },
+  first_conversion_event_name: {
+    value:
+      'Apply | Northcoders Coding Bootcamps, Leeds and Manchester: Application Form Official - November 2018'
+  },
+  hs_all_team_ids: { value: '9602' },
+  hs_social_num_broadcast_clicks: { value: '0' },
+  hs_email_recipient_fatigue_recovery_time: { value: '' },
+  hs_lifecyclestage_opportunity_date: { value: '1553619911877' },
+  ip_state_code: { value: '' },
+  hubspot_team_id: { value: '9602' },
+  hs_social_last_engagement: { value: '' },
+  recent_deal_amount: { value: '' },
+  hs_analytics_source: { value: 'ORGANIC_SEARCH' },
+  hs_email_first_open_date: { value: '1553622002645' },
+  hs_email_domain: { value: 'googlemail.com' },
+  hs_analytics_first_touch_converting_campaign: { value: '' },
+  language_learned_before_application: {
+    value: 'Another programming language (e.g. Ruby, Python, Java)'
+  },
+  hs_email_optout_4472784: { value: '' },
+  hs_email_optout_4472783: { value: '' },
+  hs_all_owner_ids: { value: '30247343' },
+  hs_calculated_phone_number: { value: '+447393199140' },
+  email: { value: 'josh@northcoders.com' },
+  hs_analytics_first_url: { value: 'https://northcoders.com/' },
+  applicant_current_situation: { value: 'Mid-career, time for a change!' },
+  hs_analytics_first_timestamp: { value: '1553260256940' },
+  hs_analytics_last_touch_converting_campaign: { value: '' },
+  engagements_last_meeting_booked: { value: '' },
+  applicants_agree_to_ts_cs: { value: 'true' },
+  applicant_aspirations: {
+    value:
+      'Aspiring to create a company that deals with Saas. Using Javascript (Preact) for the frontend and python (Flask) for the backend.\r\nI have begun my journey in learning most of my coding through w3school and Hacker Rank.\r\n\r\nI want to develop myself and become better at coding.'
+  },
+  stage: { value: 'Codewars' },
+  notes_last_contacted: { value: '1554378243000' },
+  ip_city: { value: '' },
+  hubspot_owner_assigneddate: { value: '1554378255177' },
+  hs_email_optout_4467591: { value: '' },
+  hs_social_twitter_clicks: { value: '0' },
+  hs_email_sends_since_last_engagement: { value: '0' },
+  num_notes: { value: '4' },
+  hs_email_click: { value: '3' },
+  hs_email_optout_4470787: { value: '' },
+  hs_email_last_open_date: { value: '1554740005392' },
+  hs_all_accessible_team_ids: { value: '9602' },
+  hs_email_optout_4467494: { value: '' },
+  hs_analytics_last_url: {
+    value:
+      'https://blog.northcoders.com/everything-you-need-to-know-about-your-coding-bootcamp-interview?utm_source=hs_automation&utm_medium=email&utm_content=55608470&_hsenc=p2ANqtz-9inEVpQ6c1YM8UARnTHHw58cjqq4aZ6oaVTCrJug0Agruxl-B-_6zoI6EqVsou2wHGKYQtiITQlhuzCn2WLKq4canl6A94yOw8Ce66MM5E85eBvio&_hsmi=55608470'
+  },
+  hs_email_optout_4467493: { value: '' },
+  hs_email_optout_4467495: { value: '' },
+  hs_email_quarantined: { value: '' },
+  hs_analytics_revenue: { value: '0.0' },
+  interview_booking_link: {
+    value: 'https://northcoders-entry-challenge.youcanbook.me'
+  },
+  hs_email_optout_5220126: { value: '' },
+  hs_email_optout: { value: '' },
+  hs_email_last_send_date: { value: '1554369993242' },
+  first_deal_created_date: { value: '1553619910911' },
+  hs_email_optout_4413347: { value: '' },
+  hs_email_optout_4470786: { value: '' },
+  notes_last_updated: { value: '1554378243000' },
+  hs_email_last_email_name: { value: 'Onboard IV Booked - How To Prepare' },
+  hs_ip_timezone: { value: 'europe_slash_london' },
+  hs_email_first_send_date: { value: '1553620086000' },
+  first_conversion_date: { value: '1553619890454' },
+  ip_zipcode: { value: '' },
+  interview_status: { value: '1st Interview' },
+  hs_analytics_average_page_views: { value: '1' },
+  num_conversion_events: { value: '1' },
+  ip_latlon: { value: '' },
+  heard_about_northcoders: {
+    value: 'Online: Search Engine (Google, Bing, Yahoo...)'
+  },
+  twilio_application_accepted_trigger: { value: 'true' },
+  hs_email_open: { value: '4' },
+  hs_sales_email_last_replied: { value: '' },
+  ipaddress: { value: '51.7.3.190' },
+  hs_email_first_click_date: { value: '1553627062982' },
+  applicant_coding_experience: {
+    value:
+      "I've covered the basics of a programming language (Codecademy, Introduction to Programming)"
+  },
+  firstname: { value: 'Andrew' },
+  ip_state: { value: '' },
+  ip_country: { value: 'united kingdom' },
+  cohort: { value: 'May 20th 2019' },
+  hs_analytics_last_timestamp: { value: '1554739471849' },
+  hs_analytics_num_visits: { value: '13' },
+  hs_social_linkedin_clicks: { value: '0' },
+  hs_analytics_last_visit_timestamp: { value: '1554739471849' },
+  contact_type: { value: 'B2C' },
+  num_contacted_notes: { value: '1' },
+  hs_searchable_calculated_phone_number: { value: '7393199140' },
+  notes_next_activity_date: { value: '' },
+  ip_country_code: { value: 'gb' },
+  hs_analytics_first_visit_timestamp: { value: '1553260256940' },
+  lastmodifieddate: { value: '1554747771242' },
+  hs_social_google_plus_clicks: { value: '0' },
+  hs_calculated_form_submissions: {
+    value: '26f98554-0c8c-4f92-9f2d-a734ef04bb4f::1553619890454'
+  },
+  hs_analytics_last_referrer: { value: 'android-app://com.google.android.gm' },
+  hs_email_delivered: { value: '4' },
+  chosen_location: { value: 'Manchester' },
+  hs_social_facebook_clicks: { value: '0' },
+  hs_predictivescoringtier: { value: 'tier_1' },
+  total_revenue: { value: '' },
+  recent_conversion_date: { value: '1553619890454' },
+  northcoders_interview_date: { value: '1554940800000' },
+  did_a_friend_refer_you_to_northcoders_: { value: 'false' },
+  hs_analytics_source_data_2: { value: 'GOOGLE' },
+  hs_email_optout_4413297: { value: '' },
+  hs_analytics_source_data_1: { value: 'Unknown keywords (SSL)' },
+  codewars_username: { value: 'https://www.codewars.com/users/Ziziou91111' },
+  lifecyclestage: { value: 'opportunity' }
+};
+
 const interviewObj = {
   name: 'Josh Gray',
   email: 'josh@northcoders.com',
@@ -157,6 +306,7 @@ const interviewObj = {
 };
 
 module.exports = {
-  properties,
+  hubspotContact,
+  invalidCWHubspotContact,
   interviewObj
 };
